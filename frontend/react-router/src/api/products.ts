4b6c7fbabe0c54a884129a9479a5599d70dcec5b
J Molia <alu.161050@usj.es>
2026-01-21 12:31:11 +0100

    Sistemas web probar vercel

--- frontend/react-router/src/api/products.ts
+++ frontend/react-router/src/api/products.ts
@@ -1,6 +1,6 @@
 import type { Product } from "../models/products";
 
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 export async function getProducts(): Promise<Product[]> {
   const response = await fetch(`${API_BASE_URL}/products/`);
