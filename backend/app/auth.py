4ed911c9ebd0967ed9adc5c48d37f996ca97b909
J Molia <alu.161050@usj.es>
2026-01-21 14:11:12 +0100

    Vercel registration

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -21,7 +21,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 # Security Configuration
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", bcrypt__handle_special_chars=True)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 
