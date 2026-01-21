da7c96fd4488653191baed6612f7b81aa35f69eb
J Molia <alu.161050@usj.es>
2026-01-21 14:12:43 +0100

    no va lo de antes

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -21,7 +21,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 # Security Configuration
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", bcrypt__handle_special_chars=True)
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 
