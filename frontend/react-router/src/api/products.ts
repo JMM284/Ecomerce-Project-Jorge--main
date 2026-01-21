0c7ca64c477dc07cf66a27240f1a79b85bda7c7a
J Molia <alu.161050@usj.es>
2026-01-21 12:43:26 +0100

    Vercel

--- frontend/react-router/src/api/products.ts
+++ frontend/react-router/src/api/products.ts
@@ -1,9 +1,11 @@
 import type { Product } from "../models/products";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+// const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+
+const API_BASE_URL = "https://ecomerce-project-jorge-main.onrender.com";
 
 export async function getProducts(): Promise<Product[]> {
-  const response = await fetch(`${API_BASE_URL}/products/`);
+  const response = await fetch(`${API_BASE_URL}/products`);
   if (!response.ok) {
     throw new Error("Failed to fetch products");
   }
